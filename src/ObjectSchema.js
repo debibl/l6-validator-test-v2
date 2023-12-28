8b40efff355eb9a91220e92d4728d1f11d751d1c
Vasilina <miryan.vasilina@gmail.com>
2023-12-28 09:59:10 +0300

    Update ObjectSchema.js

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,15 +1,84 @@
-export default class ObjectSchema {
-  shape(schema) {
+1)Validator.js:"import StringSchema from './StringSchema.js';
+import ArraySchema from './ArraySchema.js';
+import ObjectSchema from './ObjectSchema.js';
+
+export default class Validator {
+  string() {
+    return new StringSchema([(value) => typeof value === 'string']);
+  }
+
+  array() {
+    return new ArraySchema([(value) => Array.isArray(value)]);
+  }
+
+  object() {
+    return new ObjectSchema([(value) => typeof value === 'object']);
+  }
+}
+"
+2)StringSchema.js:"export default class StringSchema {
+  constructor(validators) {
+    this.validators = validators;
+  }
+
+  isValid(value) {
+    return this.validators.every((validator) => validator(value));
+  }
+
+  containsNumber() {
     return {
       isValid(value) {
-        if (typeof value !== 'object'   ||Array.isArray(value)  value === null) {
-          return false;
-        }
-        return Object.entries(schema).every(([key, validator]) => {
-          const propertyValue = value[key];
-          return validator && validator.isValid(propertyValue);
-        });
+        return /[0-9]/.test(value);
       },
     };
   }
 }
+"
+3)ArraySchema.js:"export default class ArraySchema {
+  constructor(validators) {
+    this.validators = validators;
+  }
+
+  isValid(value) {
+    return this.validators.every((validator) => validator(value));
+  }
+
+  containsNumber() {
+    return {
+      isValid(value) {
+        if (!Array.isArray(value)) {
+          return false;
+        }
+        return value.every((element) => /[0-9]/.test(element));
+      },
+    };
+  }
+
+  custom(val) {
+    const validator = (array) => array.every((el) => val(el));
+    return new ArraySchema([...this.validators, validator]);
+  }
+}
+"
+4)ObjectSchema:"export default class ObjectSchema {
+  constructor(validators) {
+    this.validators = [...validators];
+  }
+
+  isValid(value) {
+    return this.validators.every((validator) => validator(value));
+  }
+
+  shape(fields) {
+    const validator = (obj) => {
+      const keys = Object.keys(fields);
+      return keys.every((key) => {
+        const fieldValidator = fields[key];
+        const fieldValue = obj[key];
+        return fieldValidator.isValid(fieldValue);
+      });
+    };
+    return new ObjectSchema([...this.validators, validator]);
+  }
+}
+"
