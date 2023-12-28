1f02364f778cdfa28dacd284366d5e92ab46b0a3
Vasilina <miryan.vasilina@gmail.com>
2023-12-28 09:56:35 +0300

    Update ObjectSchema.js

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,34 +1,15 @@
 export default class ObjectSchema {
-  constructor() {
-    this.validationSchema = {};
-  }
-
   shape(schema) {
-    this.validationSchema = schema;
-    return this;
-  }
-
-  isValid(data) {
-    return this.validateObject(data, this.validationSchema);
-  }
-
-  validateObject(data, schema) {
-    for (const key of Object.keys(schema)) {
-      const validator = schema[key];
-
-      if (typeof validator === 'object' && validator !== null) {
-        // Nested object
-        if (!this.validateObject(data[key], validator)) {
+    return {
+      isValid(value) {
+        if (typeof value !== 'object'   ||Array.isArray(value)  value === null) {
           return false;
         }
-      } else {
-        // Leaf validator
-        if (!validator.validate(data[key])) {
-          return false;
-        }
-      }
-    }
-
-    return true;
+        return Object.entries(schema).every(([key, validator]) => {
+          const propertyValue = value[key];
+          return validator && validator.isValid(propertyValue);
+        });
+      },
+    };
   }
 }
